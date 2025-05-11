153f140c4b5579daafae3501f49f0d53187f2727
iridium-nitride <iridiumnitride@outlook.com>
2025-05-11 11:09:34 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function timeUpdate(){
+    const options = {
+        timeZone: 'Australia/Brisbane',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false 
+    };
+    const time = new Date().toLocaleTimeString('en-AU', options);
+    document.querySelector('#currentTime').textContent = time;
+}
+timeUpdate();
+setInterval(timeUpdate, 1000);
