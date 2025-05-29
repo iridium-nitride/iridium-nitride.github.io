f9c4ce78c3bfaaf5a9f35cadaa0f2bb680b5c9dc
iridium-nitride <iridiumnitride49@gmail.com>
2025-05-29 19:10:26 +1000

    reformed site

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-function timeUpdate(){
-    const options = {
-        timeZone: 'Australia/Brisbane',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false 
-    };
-    const time = new Date().toLocaleTimeString('en-AU', options);
-    document.querySelector('#localTime').textContent = time;
-}
-timeUpdate();
-setInterval(timeUpdate, 1000);
-
 fetch('article.html')
     .then(response => response.text())
     .then(data => {
         document.querySelector(`article`).innerHTML = data;
     })
     .catch(error => console.error(`There was an error fetching the article:`, error));
+
+fetch('aside.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector(`aside`).innerHTML = data;
+        updateLocalTime();
+        setInterval(updateLocalTime, 60000);
+    })
+    .catch(error => console.error(`There was an error fetching the aside:`, error));
+
+function updateLocalTime() {
+    const now = new Date();
+    const options = {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+      timeZone: 'Australia/Brisbane'
+    };
+    const brisbaneTime = now.toLocaleTimeString('en-AU', options);
+    document.querySelector('#localtime').textContent = brisbaneTime;
+}
